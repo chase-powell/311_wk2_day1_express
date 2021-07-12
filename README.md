70339c344f5c0abe8c6eeeecb514c15fc9eac5dc
Chase <landon.powell14@gmail.com>
2021-07-12 18:44:43 -0500

    pushing final changes

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Use Postman to test the routes created in the following sections. Your base url 
 * Create the following routes:
 
   * GET /users
-    * Give your server the ability to respond to a GET request with a path "/users" and return the users array from state.js
+    *  
     * Ex. `res.json(users)`
 
   * GET /users/1
--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
-
+app.use(bodyParser.json())
 const { users } = require('./state')
 
 /* BEGIN - create routes here */
+app.get('/', (req,res) => {
+  res.send('welcome')
+})
+app.get('/users', (req,res) => {
+  res.json(users)
+})
+app.get('/users/:id', (req,res) => {
+  res.send(users.filter(user => user._id == req.params.id))
+})
+//Post request*****
+app.post('/users', (req,res) => {
+  const counter = users.length+1
+  const newUser = {
+      "_id": counter,
+      "name": req.body.name,
+      "occupation": req.body.occupation,
+      "avatar": req.body.avatar 
+  }
+  users.push(newUser)
+  res.json(users)
+})
+//Put request****
+app.put('/users/:id', (req,res) => {
+  const editUser = {
+    "name": "Mason Cooper",
+    "occupation": "Mechanic"
+}
+users.forEach(user => {
+  if(user._id === parseInt(req.params.id)){user.name = editUser.name, 
+  user.occupation = editUser.occupation}
+})
+  res.json(users)
+})
+//Delete request
+app.delete('/users/:id', (req,res) => {
+  users.shift()
+  res.json({
+    isActive: 'false',
+    user: users.filter(user => user._id !== req.params.id)
+  })
 
+})
 
 /* END - create routes here */
 
